b0df34b01ff91aa28bb7272b32c8ddb52e8162a3
Poidog <info@poidog.co>
2017-06-10 19:45:52 -1000

    random winner selection

---
+++ app/services/contest_results.rb
@@ -0,0 +1,13 @@
+class ContestResults
+	def initialize(array)
+		raise ArgumentError.new("array is required") if array.blank?
+	end
+
+	def results(count=1)
+		if count.to_i < 2
+			@array.sample
+		else
+			@array.sample(count)
+		end
+	end
+end
