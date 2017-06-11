fee4f00235165140c6c99ee0400c3f757c334df1
Poidog <info@poidog.co>
2017-06-10 16:11:18 -1000

    Account model and related files

---
+++ app/models/account.rb
@@ -0,0 +1,6 @@
+class Account < ActiveRecord::Base
+	validates :shopify_account_url, presence: true
+	validates :shopify_api_key, presence: true
+	validates :shopify_password, presence: true
+	validates :shopify_shared_secret, presence: true
+end
